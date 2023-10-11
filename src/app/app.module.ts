9964d75ae941a2eae41880c40c9abf9800f24065
@davide.bruno <davide.bruno@edu.itspiemonte.it>
2023-10-11 12:08:07 +0200

    caricati i file importanti

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+//import { CVTemplateComponent } from './CV-template/CV-template.component';
+import{AppRoutingModule} from './app-routing.module';
+import { homeComponent } from './home/home.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+   // CVTemplateComponent
+   homeComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+})
+export class AppModule { }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+ import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class homeComponent {
+
+  hpme = true;
+  
+
+  
+}
+   
+  
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
